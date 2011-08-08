f7fd1e31163a22e0fe22a762da35e887b237f986
lnznt <lnznt0+github@gmail.com>
2011-08-08 11:58:11 +0900

    リファクタリング.

--- lib/gmrw/extension/module.rb
+++ lib/gmrw/extension/module.rb
@@ -1,42 +1,35 @@
 #!/usr/bin/env ruby
-# -*- coding :UTF-8 -*-
+# -*- coding: UTF-8 -*-
 
 require 'gmrw/extension/extension'
 
 module GMRW::Extension
   mixin Module do
-    def def_accessor(mode, name, default='')
-      no_arg      = "def #{name}"
-      va_arg      = "def #{name}(*args, &block)"
-      set_default = "instance_variable_defined?(:@#{name})" +
-                      " or (@#{name} = (#{default}))"
-      ret_val     = "@#{name}"
-      set_val     = "@#{name} = args.empty? ? (block || @#{name}) : args.first"
-      def_writer  = "attr_writer :#{name}"
+    private
+    def self.define_property(method_name, keys) #:nodoc:
+      define_method(method_name) do |name, *args|
+        default = args.empty? ? '' : args.first
 
-      module_eval case mode
-        when :ro  ; [no_arg, set_default, ret_val, "end"]
-        when :rw  ; [va_arg, set_default, set_val, "end"]
-        when :roa ; [no_arg, set_default, ret_val, "end", def_writer]
-        when :rwa ; [va_arg, set_default, set_val, "end", def_writer]
-      end * ';'
+        statements = {
+          :no_arg   => "def #{name}",
+          :va_arg   => "def #{name}(*args, &block)",
+          :default  => "instance_variable_defined?(:@#{name})" +
+                          " or (@#{name} = (#{default}))",
+          :return   => "@#{name}",
+          :set_val  => "@#{name} = args.empty? ? (block || @#{name}) : args.first",
+          :end      => "end",
+          :attr_w   => "attr_writer :#{name}",
+        }
+
+        module_eval(keys.map{|key| statements[key]} * ';')
+      end
     end
 
-    def property_ro(name, default='')
-      def_accessor:ro, name, default
-    end
-
-    def property_wr(name, default='')
-      def_accessor:wr, name, default
-    end
-
-    def property_roa(name, default='')
-      def_accessor:roa, name, default
-    end
-
-    def property_rwa(name, default='')
-      def_accessor:rwa, name, default
-    end
+    public
+    define_property :property_ro,  [:no_arg, :default, :return,  :end]
+    define_property :property_rw,  [:va_arg, :default, :set_val, :end]
+    define_property :property_roa, [:no_arg, :default, :return,  :end, :attr_w]
+    define_property :property_rwa, [:va_arg, :default, :set_val, :end, :attr_w]
 
     alias property property_rwa
   end
@@ -45,13 +38,27 @@ end
 if __FILE__ == $0
   class C
     property :foo, 10
+#    property :foo, :bar
+#    attr_accessor :bar
+
+#    def initialize
+#      foo(10000)
+#    end
   end
 
   c = C.new
+#  c.bar = 100
+#  c.bar = 200
+#  c.foo
+#  c.bar = 300
+
   p c.foo
   p c.foo = 20
   p c.foo
+  p c.foo(399)
   p c.foo
+  p c.foo {|s| "hello, #{s}" }
+  p c.foo.call("world")
 end
 
 # vim:set ts=2 sw=2 et fenc=UTF-8:
