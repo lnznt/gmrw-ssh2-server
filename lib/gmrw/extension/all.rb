ebe2439ab0914db22f03d5b1c8c3867579183ffc
lnznt <lnznt0+github@gmail.com>
2011-08-18 08:41:40 +0900

    extension/all.rb を追加.

---
+++ lib/gmrw/extension/all.rb
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+#
+# Author:: lnznt
+# Copyright:: (C) 2011 lnznt.
+# License:: Ruby's
+#
+
+require 'gmrw/extension/array'
+require 'gmrw/extension/integer'
+require 'gmrw/extension/module'
+require 'gmrw/extension/object'
+require 'gmrw/extension/string'
+
+# vim:set ts=2 sw=2 et fenc=UTF-8:
