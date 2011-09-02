b567ff22293b4e13d045c4719f01f5a2f075581e
lnznt <lnznt0+github@gmail.com>
2011-09-02 17:16:20 +0900

    パスワード認証の実装(簡易版)

--- lib/gmrw/ssh2/server/userauth.rb
+++ lib/gmrw/ssh2/server/userauth.rb
@@ -7,13 +7,14 @@
 
 require 'gmrw/extension/all'
 require 'gmrw/utils/loggable'
+require 'gmrw/ssh2/server/userauth/password_auth'
 
 module GMRW; module SSH2; module Server; class UserAuth
   include GMRW
   include Utils::Loggable
 
   def_initialize :service
-  forward [:logger, :die, :services, :send_message] => :service
+  forward [:logger, :die, :send_message] => :service
   
   def start(service_name)
     debug( "in service: #{service_name}" )
@@ -21,18 +22,28 @@ module GMRW; module SSH2; module Server; class UserAuth
     service.add_observer(:userauth_request, &method(:userauth_request_received))
   end
 
-  #############################################################
-  #
-  # DUMMY
-  #
+  property_ro :authentications, %-
+  {
+    "password" => PasswordAuth.new(self),
+  }
+  -
+
+  def welcome(opts)
+    service.notify_observers(opts[:service_name], opts[:service_name])
+
+    send_message :userauth_banner,
+                 :message => "\r\nWelcome to GMRW SSH2 Server\r\n\r\n"
+    send_message :userauth_success
+  end
+
+  def please_retry(partial_success=false)
+    send_message :userauth_failure, :auths_can_continue => ['password'],
+                                    :partial_success    => partial_success
+  end
+
   def userauth_request_received(message, *a)
-    case message[:method_name]
-      when 'none'
-        send_message :userauth_failure, :auths_can_continue => ['password']
-      when 'password'
-        service.notify_observers(message[:service_name], message[:service_name])
-        send_message :userauth_success
-    end
+    auth = authentications[message[:method_name]]
+    auth ? auth.authenticate(message) : please_retry
   end
 end; end; end; end
 
