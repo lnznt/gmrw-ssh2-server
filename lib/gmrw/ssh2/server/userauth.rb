9bee5588c8bfcb19ac65ae7105b48e7bb2aa673f
lnznt <lnznt0+github@gmail.com>
2011-09-03 09:41:27 +0900

    認証のタイムアウト、リトライカウント実装

--- lib/gmrw/ssh2/server/userauth.rb
+++ lib/gmrw/ssh2/server/userauth.rb
@@ -7,6 +7,7 @@
 
 require 'gmrw/extension/all'
 require 'gmrw/utils/loggable'
+require 'gmrw/ssh2/server/userauth/user'
 require 'gmrw/ssh2/server/userauth/password_auth'
 
 module GMRW; module SSH2; module Server; class UserAuth
@@ -22,26 +23,38 @@ module GMRW; module SSH2; module Server; class UserAuth
     service.add_observer(:userauth_request, &method(:userauth_request_received))
   end
 
+  property_ro :banner, '"\r\nWelcome to GMRW SSH2 Server\r\n\r\n"'
+  property_ro :user, 'User.new(self)'
+
   property_ro :authentications, %-
   {
     "password" => PasswordAuth.new(self),
   }
   -
 
+  #
+  # :section: reply message
+  #
   def welcome(opts)
     service.notify_observers(opts[:service_name], opts[:service_name])
 
-    send_message :userauth_banner,
-                 :message => "\r\nWelcome to GMRW SSH2 Server\r\n\r\n"
+    send_message :userauth_banner, :message => banner
     send_message :userauth_success
   end
 
   def please_retry(partial_success=false)
+    user.count_check!
+
     send_message :userauth_failure, :auths_can_continue => ['password'],
                                     :partial_success    => partial_success
   end
 
+  #
+  # :section: message handling
+  #
   def userauth_request_received(message, *a)
+    user.name_check!(message)
+
     message_catalog.auth = message[:method_name]
     auth = authentications[message[:method_name]]
     auth ? auth.authenticate(message) : please_retry
