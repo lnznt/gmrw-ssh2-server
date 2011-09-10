c41005354ffd5ace9b865334abf9b02a6cda5403
lnznt <lnznt0+github@gmail.com>
2011-09-10 18:01:20 +0900

    リファクタリング.

--- lib/gmrw/ssh2/message/datatype.rb
+++ lib/gmrw/ssh2/message/datatype.rb
@@ -26,49 +26,49 @@ module GMRW; module SSH2; module Message
                         :is_type  => this.is.boolean?,
                         :encode   => proc { (this ? 1 : 0).pack.byte },
                         :separate => proc { this / 1 },
-                        :decode   => proc { this.to.byte != 0 },
+                        :decode   => proc {|s| s.to.byte != 0 },
         },
         :byte     => {  :default  => 0,
                         :is_type  => this.is.byte?,
                         :encode   => proc { this.pack.byte },
                         :separate => proc { this / 1 },
-                        :decode   => proc { this.to.byte },
+                        :decode   => proc {|s| s.to.byte },
         },
         :uint32   => {  :default  => 0,
                         :is_type  => this.is.uint32?,
                         :encode   => proc { this.pack.uint32 },
                         :separate => proc { this / 4 },
-                        :decode   => proc { this.to.uint32 },
+                        :decode   => proc {|s| s.to.uint32 },
         },
         :uint64   => {  :default  => 0,
                         :is_type  => this.is.uint64?,
                         :encode   => proc { this.pack.uint64 },
                         :separate => proc { this / 8 },
-                        :decode   => proc { this.to.uint64 },
+                        :decode   => proc {|s| s.to.uint64 },
         },
         :mpint    => {  :default  => 0,
                         :is_type  => this.kind_of?(OpenSSL::BN),
                         :encode   => proc { this.to_i.pack.bin.to.packet },
                         :separate => proc { this[4..-1] / this.unpack("N")[0] },
-                        :decode   => proc { this.to.mpi.to.bn },
+                        :decode   => proc {|s| s.to.mpi.to.bn },
         },
         :string   => {  :default  => "",
                         :is_type  => this.is.string?,
                         :encode   => proc { this.bin.to.packet },
                         :separate => proc { this[4..-1] / this.unpack("N")[0] },
-                        :decode   => proc { this },
+                        :decode   => proc {|s| s },
         },
         :namelist => {  :default  => [],
                         :is_type  => this.is.array? {|s| name?(s) },
                         :encode   => proc { this.join(",").bin.to.packet },
                         :separate => proc { this[4..-1] / this.unpack("N")[0] },
-                        :decode   => proc { this.split(",") },
+                        :decode   => proc {|s| s.split(",") },
         },
         :bytes    => {  :default  => [0] * n,
                         :is_type  => this.is.array?(n) {|b| b.is.byte? },
                         :encode   => proc { this.pack("C*") },
                         :separate => proc { this / n },
-                        :decode   => proc { this.to.bytes },
+                        :decode   => proc {|s| s.to.bytes },
         },
       }[type] || {}
     end
@@ -86,13 +86,9 @@ module GMRW; module SSH2; module Message
       spec(type)[:encode].call
     end
 
-    def dec(type)
-      spec(type)[:decode].call
-    end
-
     def decode(type)
       s, rem = spec(type)[:separate].call
-      [s.ssh.dec(type), rem]
+      [spec(type)[:decode][s], rem]
     end
 
     def pack
