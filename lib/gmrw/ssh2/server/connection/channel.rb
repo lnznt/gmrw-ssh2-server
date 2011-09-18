94507b7c20126d8a1be9623e94f6cd6641394274
lnznt <lnznt0+github@gmail.com>
2011-09-18 10:22:13 +0900

    リファクタリング

--- lib/gmrw/ssh2/server/connection/channel.rb
+++ lib/gmrw/ssh2/server/connection/channel.rb
@@ -13,6 +13,60 @@ module GMRW; module SSH2; module Server; class Connection
     include SSH2::Loggable
     forward [:die, :logger, :at_close] => :service
 
+    property :remote_channel
+
+    def reply(tag, params={})
+      service.send_message tag, { :recipient_channel => remote_channel }.merge(params)
+    end
+
+    def open(message)
+      local_channel  = service.channels.pop
+      remote_channel   message[:sender_channel]
+      window.unit_size message[:maximum_packet_size]
+      window.push      message[:initial_window_size]
+
+      info( "channel open: ##{local_channel} -- remote##{remote_channel}" )
+
+      closing do
+        info( "channel close: ##{local_channel}" )
+        reply :channel_close
+        service.cancel [:channel, local_channel]
+        service.channels.push local_channel
+        program.shutdown
+      end
+
+      service.register [:channel, local_channel] => method(:message_received)
+
+      reply :channel_open_confirmation,
+            :sender_channel      => local_channel,
+            :initial_window_size => local_window_size.init,
+            :maximum_packet_size => local_window_size.unit
+    end
+
+    property :closing
+
+    def close(*)
+      closing.call
+    end
+
+    def message_received(message)
+      handler = {
+        :channel_request       => method(:request),
+        :channel_window_adjust => proc {|msg| window.push msg[:bytes_to_add] },
+        :channel_data          => method(:write_data),
+        :channel_extended_data => method(:write_data),
+        :channel_eof           => method(:close),
+        :channel_close         => method(:close),
+      }[message.tag]
+
+      handler ? handler[message] : error( "message not handling #{message.tag}" )
+    end
+
+    #
+    # :section: window size adjust
+    #
+    property_ro :window, 'Window.new'
+
     class Window < Queue
       property :unit_size
 
@@ -22,72 +76,30 @@ module GMRW; module SSH2; module Server; class Connection
       end
     end
 
-    property_ro :window, 'Window.new'
-
-    property_ro :local_channel, 'service.channels.pop'
-    property_ro :event,         '[:channel, local_channel]'
-
-    property :remote_channel
-
-    def reply(tag, params={})
-      service.send_message tag, { :recipient_channel => remote_channel }.merge(params)
-    end
-
-    def open(message)
-      remote_channel   message[:sender_channel]
-      window.unit_size message[:maximum_packet_size]
-      window.push      message[:initial_window_size]
-
-      service.register event => method(:message_received)
-
-      reply :channel_open_confirmation,
-            :sender_channel      => local_channel,
-            :initial_window_size => initial_window_size,
-            :maximum_packet_size => maximum_packet_size
-
-      info( "channel open: (remote)[#{remote_channel}]---[#{local_channel}](local)" )
-    end
-
-    property :handlers, '{
-      :channel_request       => method(:request),
-      :channel_window_adjust => method(:window_adjust),
-      :channel_data          => method(:write_data),
-      :channel_extended_data => method(:write_data),
-      :channel_eof           => method(:close),
-      :channel_close         => method(:close),
-    }'
-
-    def message_received(message)
-      handler = handlers[message.tag]
-      handler ? handler.call(message) : error( "message not handling #{message.tag}" )
-    end
-
-    def close(*)
-      info( "channel close: (remote)[#{remote_channel}]---[#{local_channel}](local)" )
-      reply :channel_close
-      service.cancel event
-      service.channels.push local_channel
-      program.shutdown
-    end
-
-    property_ro :initial_window_size, '1024 * 1024'
-    property_ro :maximum_packet_size, '  16 * 1024'
-    property :window_size, :initial_window_size
-
+    #
+    # :section: receive data
+    #
     def write_data(message)
-      data = message[:data]
-      program.write data
-      window_size(window_size - data.length)
-      window_size >= maximum_packet_size or charge_window
+      program.write message[:data]
+      local_window_size.consume(message[:data].length)
     end
 
-    def charge_window
-      reply :channel_window_adjust, :bytes_to_add => initial_window_size - window_size
-      window_size = initial_window_size
-    end
+    property :local_window_size, 'LocalWindowSize.new(self)'
 
-    def window_adjust(message)
-      window.push message[:bytes_to_add]
+    class LocalWindowSize
+      def_initialize :service
+      property_ro :init, '1024 * 1024'
+      property_ro :unit, '  16 * 1024'
+      property    :size, '0'
+
+      def consume(n)
+        size(size - n) >= unit or charge
+      end
+
+      def charge
+        service.reply :channel_window_adjust, :bytes_to_add => init - size
+        size(init)
+      end
     end
   end
 end; end; end; end
